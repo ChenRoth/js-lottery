032cf1eb459d717b7b6bc689f88691db8df58e85
Chen Roth <rothchen@gmail.com>
2021-05-15 15:59:57 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const LOTTERY_RANGE = 20;
+const REQUIRED_NUMBERS_COUNT = 4;
+const MAX_LOTTERY_FORMS = 5;
+
+const state = {
+
+}
+
+main();
+function main() {
+
+}
+
+function createLotteryForm() {
+    const formEl = document.createElement('form');
+    Array.from(Array(LOTTERY_RANGE))
+}
